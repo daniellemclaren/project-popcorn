10ece04069ac7bf2ae7c276ac004607326402336
Danni <53468355+daniellemclaren@users.noreply.github.com>
2025-07-13 15:27:24 +0100

    test(TicketService): add test for calling payment and reservation services

--- test/unit/pairtest/TicketService.test.js
+++ test/unit/pairtest/TicketService.test.js
@@ -12,8 +12,37 @@ beforeEach(() => {
 
 describe('TicketService', () => {
   it('throws an error if accountId is invalid', () => {
-    expect(() =>
-      ticketService.purchaseTickets(0, new TicketTypeRequest('ADULT', 1))
-    ).toThrow('Invalid account ID');
+    expect(() => ticketService.purchaseTickets(0, new TicketTypeRequest('ADULT', 1))).toThrow(
+      'Invalid account ID'
+    );
+  });
+
+  it('calls payment and reservation services with correct values', () => {
+    const accountId = 123;
+
+    const requests = [
+      new TicketTypeRequest('ADULT', 2),
+      new TicketTypeRequest('CHILD', 1),
+      new TicketTypeRequest('INFANT', 1),
+    ];
+
+    const expectedAmount =
+      2 * TICKET_TYPES.ADULT.price + 1 * TICKET_TYPES.CHILD.price + 1 * TICKET_TYPES.INFANT.price;
+
+    const expectedSeats =
+      (TICKET_TYPES.ADULT.seatRequired ? 2 : 0) +
+      (TICKET_TYPES.CHILD.seatRequired ? 1 : 0) +
+      (TICKET_TYPES.INFANT.seatRequired ? 1 : 0);
+
+    ticketService.purchaseTickets(accountId, ...requests);
+
+    expect(ticketService.paymentService.makePayment).toHaveBeenCalledWith(
+      accountId,
+      expectedAmount
+    );
+    expect(ticketService.reservationService.reserveSeat).toHaveBeenCalledWith(
+      accountId,
+      expectedSeats
+    );
   });
 });
